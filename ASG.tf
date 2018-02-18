fba0b04cc173aaa658c4f5a2c44b8a2acfd51047
Vivek Bansal <vickybansal@users.noreply.github.com>
2018-02-18 22:41:23 +0530

    Add files via upload

---
+++ ASG.tf
@@ -0,0 +1,30 @@
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-40d28157"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -5,9 +10,36 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p "${var.server_port}" &
+                EOF
 
   tags {
     Name = "ExampleleServer1",
     Use = "Demo for me"
+    Env = "dev"
+    UAI = "UAI2009267"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }

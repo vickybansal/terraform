89673d16008934da64dffae71c0eda84b03951fa
Vivek Bansal <vickybansal@users.noreply.github.com>
2018-02-18 23:12:19 +0530

    Add files via upload

--- ASG.tf
+++ ASG.tf
@@ -1,3 +1,4 @@
+data "aws_availability_zones" "all" {}
 
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-40d28157"
@@ -18,6 +19,10 @@ resource "aws_launch_configuration" "example" {
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  load_balancers    = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -27,4 +32,45 @@ resource "aws_autoscaling_group" "example" {
     value               = "terraform-asg-example"
     propagate_at_launch = true
   }
+}
+
+resource "aws_elb" "example" {
+  name               = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups    = ["${aws_security_group.elb.id}"]
+
+  listener {
+      lb_port           = 80
+      lb_protocol       = "http"
+      instance_port     = "${var.server_port}"
+      instance_protocol = "http"
+    }
+
+    health_check {
+      healthy_threshold   = 2
+      unhealthy_threshold = 2
+      timeout             = 3
+      interval            = 30
+      target              = "HTTP:${var.server_port}/"
+    }
+}
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
 }
